ea503fb65efcd31688b489efba682a4163cbd35b
boabner <boabner@gmail.com>
2020-09-13 08:58:07 -0400

    first commit

---
+++ src/main/java/com/treeceSpringBoot/treeceSpringBootConfiguration/TreeceSpringBootConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.treeceSpringBoot.treeceSpringBootConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeceSpringBootConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeceSpringBootConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treeceSpringBoot/treeceSpringBootConfiguration/TreeceSpringBootConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treeceSpringBoot.treeceSpringBootConfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeceSpringBootConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
